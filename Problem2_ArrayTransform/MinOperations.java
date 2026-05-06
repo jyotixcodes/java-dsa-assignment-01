1836878b2af08342da13379424ca47a3f9c8166e
jyotixcodes <csepampa@gmail.com>
2026-05-06 15:41:10 +0530

    Refactor minOps method for array transformation

--- Problem2_ArrayTransform/MinOperations.java
+++ Problem2_ArrayTransform/MinOperations.java
@@ -1,38 +1,33 @@
 import java.util.*;
 
 public class MinOperations {
-
     public static int minOps(int[] A, int K) {
-        int n = A.length;
-
-        int rem = A[0] % K;
-
+        
         for (int x : A) {
-            if (x % K != rem) return -1;
+            if ((x - A[0]) % K != 0) {
+                return -1; // Not possible
+            }
         }
-
-        int[] B = new int[n];
-
+        int n = A.length;
+        int[] steps = new int[n];
         for (int i = 0; i < n; i++) {
-            B[i] = (A[i] - rem) / K;
+            steps[i] = A[i] / K;
+        }
+        Arrays.sort(steps);
+
+        int median = steps[n / 2];
+
+        int totalOps = 0;
+        for (int x : steps) {
+            totalOps += Math.abs(x - median);
         }
 
-        Arrays.sort(B);
-
-        int median = B[n / 2];
-        int ops = 0;
-
-        for (int x : B) {
-            ops += Math.abs(x - median);
-        }
-
-        return ops;
+        return totalOps;
     }
 
     public static void main(String[] args) {
         int[] A = {2, 4, 6, 8, 10};
         int K = 2;
-
-        System.out.println(minOps(A, K)); // 6
+        System.out.println("Minimum Operations: " + minOps(A, K)); // 6
     }
 }
